c6fcb87f507555ddff5df36ace90c254d8fd4090
senpaioback <tiiodosnoobs@gmail.com>
2026-07-16 13:53:58 -0300

    first commit

---
+++ 15072026/atividade2.py
@@ -0,0 +1,4 @@
+nome = input("Qual seu nome? ")
+idade = int(input("Qual sua idade? "))
+peso = float(input("Qual seu peso? "))
+print(f"Bem Vindo {nome}! Registramos sua idade: {idade} e Peso: {peso:.2f}")
---
+++ 16072026/atividade1.py
@@ -0,0 +1,4 @@
+nome = input("Qual seu nome? ")
+
+# format: jeito antigo de fazer. moderno seria colocar a varivel dentro do {}. exemplo: ...{Nome}...
+print("Prazer em conhecer, {}!".format(nome))
